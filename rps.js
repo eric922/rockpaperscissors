7602747022bcd3e78219303a9394eb898bb7325f
eric922 <eric92289@gmail.com>
2023-05-15 16:49:07 -0400

    Added the function that plays a round of the game.

--- rps.js
+++ rps.js
@@ -9,8 +9,28 @@ function getComputerChoice(){
     else{
         choice = "scissors";
     }
-    console.log(choice)
+    //console.log(choice)
     return choice
 }
-
-getComputerChoice()
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        console.log("This round is a tie")
+    }
+    else if(playerSelection == 'rock' && computerSelection == "paper"){
+        console.log("You chose rock and the computer chose paper. You lose")
+    }
+    else if(playerSelection == "paper" && computerSelection == "scissors"){
+        console.log("You chose paper and the computer chose scissors. You lose")
+    }
+    else if(playerSelection == "scissors" && computerSelection == "rock"){
+        console.log("You chose scissors and the computer chose rock. You lose")
+    }
+    else{
+        console.log("You win!")
+    }
+}
+ComputerChoice = getComputerChoice();
+playerChoice = prompt("Please enter your selection").toLowerCase()
+console.log(playerChoice)
+console.log(ComputerChoice);
+playRound(playerChoice, ComputerChoice)
